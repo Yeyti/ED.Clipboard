577ee4a0d5cd16e0f70083156645eaa2a386477c
ED inc <yeyti@yandex.ru>
2017-09-13 12:54:02 +0300

    Base functional

---
+++ ED.CrossPlatform.Clipboard/Clipboard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GwenNet.Platform
+{
+    public class Clipboard{
+        private static string buf;
+
+        public static void Copy(string val){
+            if (OS.IsWindows()){
+                $"echo {val} | clip".Bat();
+            }
+
+            if (OS.IsMacOS()){
+                $"echo \"{val}\" | pbcopy".Bash();
+            }
+        }
+
+        
+        public static bool ContainsText()
+        {
+            /*if (OS.IsWindows())
+            {
+                buf = $"echo {val} | clip".Bat();
+            }*/
+
+            if (OS.IsMacOS())
+            {
+                buf = $"echo pbpaste".Bash();
+            }
+            
+            return buf != "";
+        }
+
+        public static string GetText(){
+            return buf;
+        }
+    }
+}
---
+++ ED.CrossPlatform.Clipboard/OS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace GwenNet.Platform{
+    class OS{
+        public static bool IsWindows() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static bool IsMacOS() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static bool IsLinux() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+    }
+}
